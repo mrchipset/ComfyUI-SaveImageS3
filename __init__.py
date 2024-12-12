39d51d743afb0b15e146ff788727e30c05c58f3d
Mr.Chip <mrchip.zxf@outlook.com>
2024-12-12 11:02:44 +0800

    DOC: add file header

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+"""
+@author: Mr.Chip
+@title: SaveImageS3
+@nickname: SaveImageS3
+@description: This extension offers a custom node to save image to S3-compatible oss.
+"""
 from .nodes.images import SaveImageS3
 from .server import *
 
